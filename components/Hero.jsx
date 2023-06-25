ea79dab225383286a78ae11db98c26192a7db9c7
prathviraj b <pishere.learn@gmail.com>
2023-06-25 18:58:23 +0530

    ui update

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -28,23 +28,9 @@ const Hero = () => {
         </div>
       </div>
       <div className="absolute xs:bottom-12 bottom-10 w-full flex flex-col justify-center items-center">
-        <a href="https://drive.google.com/file/d/1Fj2b0SzR7ZGK-UDUM6CufzXNG-MrTqC1/view?usp=sharing" target="_blank" className="text-white/70 box sm:text-xl sm:max-w-full max-w-[120px] cursor-pointer hover:text-white/90 transition-colors">Checkout My <span className="myunderline">Resume</span></a>
-        <a className="cursor-pointer z-10" href="#about">
-          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
-            <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
-              transition={{
-                duration: 1.5,
-                repeat: Infinity,
-                repeatType: "loop",
-              }}
-              className="w-3 h-3 rounded-full bg-secondary mb-1"
-            />
-          </div>
-        </a>
+        <a href="https://drive.google.com/file/d/1Fj2b0SzR7ZGK-UDUM6CufzXNG-MrTqC1/view?usp=sharing" target="_blank" className="text-white/70 box sm:text-xl sm:max-w-full p-5 max-w-[128px] cursor-pointer hover:text-white/90 transition-colors">Checkout My <span className="myunderline">Resume</span></a>
       </div>
+      <div className="absolute sm:bottom-10 bottom-1 flex w-screen items-center justify-center text-2xl animate-pulse">&#x2304;</div>
     </section>
   );
 };
